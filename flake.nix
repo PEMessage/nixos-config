3521a0628c7a3b5637f3fc654da7db7dc016e998
PEMessage <1165739182@qq.com>
2026-04-28 03:24:34 +0000

    init: add editorconfig, add build relate packages, neovim use unstable

--- flake.nix
+++ flake.nix
@@ -3,7 +3,8 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
-    nixos-wsl = { 
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixos-wsl = {
       url = "github:nix-community/NixOS-WSL/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
@@ -13,21 +14,47 @@
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }@inputs: 
-  let
+  outputs = { self, nixpkgs, nixpkgs-unstable, home-manager, ... }@inputs:
+    let
+
+    overlay = [
+      (final: prev: rec {
+        neovim = inputs.nixpkgs-unstable.legacyPackages.${prev.system}.neovim;
+      })
+    ];
+    overrideModule = {
+      nixpkgs.overlays = overlay;
+      nixpkgs.config.allowUnfree = true;
+    };
     commonModule = { config, pkgs, ... }: {
       nix.settings.experimental-features = [ "nix-command" "flakes" ];
-      
-      environment.systemPackages = with pkgs; [ 
+
+      environment.systemPackages = with pkgs; [
         git
         vim
         tmux
         python3
         zsh
+        wget
+        curl
+        neovim
+
+        # build
+        gcc
+        gnumake
+        cmake
+        pkg-config
       ];
+
       imports = [
         home-manager.nixosModules.home-manager
       ];
+
+      # Could not start dynamically linked executable: tree-sitter
+      # NixOS cannot run dynamically linked executables intended for generic
+      # linux environments out of the box. For more information, see:
+      # https://nix.dev/permalink/stub-ld
+      programs.nix-ld.enable = true;
 
       home-manager =  {
         useGlobalPkgs = true;
@@ -37,14 +64,15 @@
         };
       };
     };
-  in {
-    nixosConfigurations.wsl = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      specialArgs = { inherit (inputs) nixos-wsl; };
-      modules = [
-        commonModule
-        ./machines/wsl.nix
-      ];
+    in {
+      nixosConfigurations.wsl = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = { inherit (inputs) nixos-wsl; };
+        modules = [
+          overrideModule
+            commonModule
+            ./machines/wsl.nix
+        ];
+      };
     };
-  };
 }
